79eca4bd588395f029ec9e550af18878c6ed1aa9
Andrew Minion <andrew@andrewrminion.com>
2016-10-27 16:17:19 -0400

    add LSTA credit

--- footer.php
+++ footer.php
@@ -18,15 +18,17 @@
 
     <footer id="colophon" class="site-footer" role="contentinfo">
         <div class="site-info">
-            <?php
-                /**
-                 * Fires before the Twenty Fifteen footer text for footer customization.
-                 *
-                 * @since Twenty Fifteen 1.0
-                 */
-                do_action( 'twentyfifteen_credits' );
-            ?>
-            <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyfifteen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyfifteen' ), 'WordPress' ); ?></a>
+            <p>
+                <?php
+                    /**
+                     * Fires before the Twenty Fifteen footer text for footer customization.
+                     *
+                     * @since Twenty Fifteen 1.0
+                     */
+                    do_action( 'twentyfifteen_credits' );
+                ?>
+                <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyfifteen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyfifteen' ), 'WordPress' ); ?></a></p>
+            <p>LSTA grant programs are made possible by grant funds from the Institute of Museum and Library Services under the provisions of the federal Library Services and Technology Act as administered by the State Library of North Carolina, a division of the Department of Cultural Resources.</p>
         </div><!-- .site-info -->
     </footer><!-- .site-footer -->
 
